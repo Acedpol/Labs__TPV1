f1a3781fd4efbb5fe3ff5150fc9ff4835ed4cd00
Marcos Llinares Montes <marcolli@ucm.es>
2020-10-18 19:48:46 +0200

    Lista de Alquileres añadido y sus funciones. El sort no funciona

--- Lab.n2/Lab.n2.cpp
+++ Lab.n2/Lab.n2.cpp
@@ -2,10 +2,17 @@
 #include <iostream> // entrada/salida, define las variables cin y cout (console)
 #include <string> // cadenas de caracteres de la biblioteca estándar (STL)
 #include <fstream> // entrada/salida de ficheros de texto
+#include <algorithm> // para el sort
 
 using namespace std; /* para nombrar sin cualificar con std:: */
 
 #pragma region STRUCTS
+struct fecha
+{
+	string ano;
+	string mes;
+	string dia;
+};
 
 struct Coche {
 	int codigo;
@@ -17,7 +24,22 @@ struct Coche {
 struct ListaCoches {
 	Coche* cars; // array dinamico
 	int tam = 0; // tamaño escalable
+	int cont = 0; // Numero real de elementos
 };
+
+struct Alquiler {
+	int codigo;
+	fecha f;
+	int numDias;
+	Coche* puntCoche; //puntero al coche al que hace referencia
+};
+
+struct ListaAlquileres {
+	Alquiler* rents; // array dinamico
+	int tam = 0; // tamaño escalable
+	int cont = 0; // Numero real de elementos
+};
+
 
 #pragma endregion
 
@@ -33,6 +55,16 @@ void mostrarListaC(ListaCoches& listaC)
 	}
 }
 
+void mostrarListaA(ListaAlquileres& listaA)
+{
+	int i = 0;
+	while (i < listaA.tam)
+	{
+		cout << i << " -> " << listaA.rents[i].codigo << ' ' << listaA.rents[i].f.ano << '-' << listaA.rents[i].f.mes << '-' << listaA.rents[i].f.dia << ' ' << listaA.rents[i].numDias << endl;
+		i++;
+	}
+}
+
 bool cargarCoches(string const& fichEntrada, ListaCoches& listaC) 
 {
 	ifstream input;
@@ -41,7 +73,8 @@ bool cargarCoches(string const& fichEntrada, ListaCoches& listaC)
 	if (input.is_open()) //throw(Error(“No se encuentra el fichero”)); -> esto sobra, con el if salva la excepcion
 	{
 		int cont;
-		input >> listaC.tam; // hay 'X' coches
+		input >> listaC.tam; // hay 'X' coches, Sumar 10?
+		listaC.tam += 10;
 		listaC.cars = new Coche[listaC.tam];
 		int i = 0;
 		while (!input.fail() && i < listaC.tam) 
@@ -50,10 +83,9 @@ bool cargarCoches(string const& fichEntrada, ListaCoches& listaC)
 			input >> listaC.cars[i].precio;
 			getline(input, listaC.cars[i].modelo); // asi es como se usa :)
 			i++;
+			listaC.cont++;
 		}
 		input.close();
-		mostrarListaC(listaC);
-		//delete[] listaC.cars;
 		return true;
 	} 
 	else 
@@ -78,17 +110,89 @@ Coche* buscarCoche(int code, ListaCoches& listaC)
 	return c;
 }
 
+bool leerAlquileres(string const& fichEntrada, ListaAlquileres& listaA, ListaCoches& listaC)
+{
+	ifstream input;
+	input.open(fichEntrada);	
+
+	if (input.is_open()) //throw(Error(“No se encuentra el fichero”)); -> esto sobra, con el if salva la excepcion
+	{
+		input >> listaA.tam; // hay 'X' coches
+		listaA.tam += 10;
+		listaA.rents = new Alquiler[listaA.tam];
+		int i = 0;
+		while (!input.fail() && i < listaA.tam)
+		{
+			input >> listaA.rents[i].codigo;
+			//input >> listaA.rents[i].fechaString;
+			getline(input, listaA.rents[i].f.ano, '/');
+			getline(input, listaA.rents[i].f.mes, '/');
+			getline(input, listaA.rents[i].f.dia, ' ');
+			input >> listaA.rents[i].numDias;
+			listaA.rents[i].puntCoche = buscarCoche(listaA.rents[i].codigo, listaC);
+			i++;
+			listaA.cont++;
+		}
+		input.close();
+		return true;
+	} 
+	else 
+	{
+		input.close();
+		return false;
+	}	
+}
+
+
+//bool Comparador(const fecha& f1,const fecha& f2) {
+//	//aqui
+//	int f1Ano, f1Mes, f1Dia, f2Ano, f2Mes, f2Dia;
+//	f1Ano = stoi(f1.ano);
+//	f1Mes = stoi(f1.mes);
+//	f1Dia = stoi(f1.dia);
+//	f2Ano = stoi(f2.ano);
+//	f2Mes = stoi(f2.mes);
+//	f2Dia = stoi(f2.dia);
+//	return (f1Ano < f2Ano ||
+//	       (f1Ano == f2Ano && f1Mes < f2Mes || 
+//		   (f1Mes == f2Mes && f1Dia < f2Dia)));
+//}
+bool Comparador(const Alquiler f1, const Alquiler f2) {
+	//aqui
+	int f1Ano, f1Mes, f1Dia, f2Ano, f2Mes, f2Dia;
+	f1Ano = stoi(f1.f.ano);
+	f1Mes = stoi(f1.f.mes);
+	f1Dia = stoi(f1.f.dia);
+	f2Ano = stoi(f2.f.ano);
+	f2Mes = stoi(f2.f.mes);
+	f2Dia = stoi(f2.f.dia);
+	return (f1Ano < f2Ano ||
+	       (f1Ano == f2Ano && f1Mes < f2Mes || 
+		   (f1Mes == f2Mes && f1Dia < f2Dia)));
+}
+
+void ordenarAlquileres(ListaAlquileres& listaA)
+{
+	sort(&listaA.rents[0], &listaA.rents[listaA.cont], Comparador);
+	mostrarListaA(listaA);
+}
+
 #pragma endregion
 
 int main(int argc, char* argv[]) // Argumentos: Array de cadenas estilo C
 { 
 	ListaCoches listaC;
+	ListaAlquileres listaA;
 	string file = "coches.txt";
+	string file2 = "rent.txt";
 
 	cargarCoches(file, listaC);
-	cout << "punteroCoche: " << buscarCoche(1200, listaC) << endl;
-	cout << "punteroCoche2: " << buscarCoche(1325, listaC) << endl;
+	leerAlquileres(file2, listaA, listaC);
+	ordenarAlquileres(listaA);
+	/*cout << "punteroCoche: " << buscarCoche(1200, listaC) << endl;
+	cout << "punteroCoche2: " << buscarCoche(1325, listaC) << endl;*/
 	delete[] listaC.cars;
+	delete[] listaA.rents;
 
 	system("pause"); // -> módulo utilsSystem
 	return 0;
