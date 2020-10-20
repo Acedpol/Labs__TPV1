0b1202eee6ab0794e2e52f28018e71ee35f31b01
Pablo Cubells <pablocub@ucm.es>
2020-10-20 19:59:16 +0200

    Esquema inicial del menú
    ¡ .sln ha cambiado ! -> Ahora sólo abre Lab.n3 pero todo sigue en su sitio.

--- Lab.n3/Main.cpp
+++ Lab.n3/Main.cpp
@@ -16,6 +16,40 @@ int main(int argc, char* argv[]) // Argumentos: Array de cadenas estilo C
 	string file = "coches.txt";
 	string file2 = "rent.txt";
 
+	// MENU PRINCIPAL:
+	cout << "~ MENU PRINCIPAL ~" << endl << endl
+		<< "Escoge una opcion: " << endl
+		<< " 1 -> Mostrar Coches " << endl
+		<< " 2 -> Mostrar Alquileres " << endl
+		<< " 3 -> Agregar Coche " << endl
+		<< " 4 -> Agregar Alquiler " << endl
+		<< " 5 -> Salir del Programa " << endl << endl;
+
+	int opt;
+	cout << "Entrada usuario: ";
+	cin >> opt;
+	--opt; // ¿es igual que opt--?
+	switch (opt)
+	{
+	case 0:
+		cout << "Mostrar Coches" << endl;
+		break;
+	case 1:
+		cout << "Mostrar Alquileres" << endl;
+		break;
+	case 2:
+		cout << "Agregar Coche" << endl;
+		break;
+	case 3:
+		cout << "Agregar Alquiler" << endl;
+		break;
+	case 4:
+		cout << "Salir del Programa" << endl;
+		break;
+	default:
+		break;
+	}
+
 	//cargarCoches(file, listaC);
 	//leerAlquileres(file2, listaA, listaC);
 	//ordenarSinSort(listaA);
