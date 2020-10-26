6f6e2bcfb338b1ad0803ead5741b05c53b4ef9e6
Pablo Cubells <pablocub@ucm.es>
2020-10-27 00:54:31 +0100

    Creación: Proyecto n.4___________________

---
+++ Lab.n4/Main.cpp
@@ -0,0 +1,15 @@
+// Importamos las cabeceras de los modulos que necesitamos
+#include <iostream> // entrada/salida, define las variables cin y cout (console)
+#include <string> // cadenas de caracteres de la biblioteca estandar (STL)
+
+using namespace std; /* para nombrar sin cualificar con std:: */
+
+#pragma region METODOS
+
+int main(int argc, char* argv[]) // Argumentos: Array de cadenas estilo C
+{
+	system("pause"); // -> modulo utilsSystem
+	return 0;
+}
+
+#pragma endregion
